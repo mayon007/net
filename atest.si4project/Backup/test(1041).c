cf493a06cd26eb1198d699480b1147c100ee99c3
mayon007 <maoyongf2008@126.com>
2017-12-21 14:25:16 +0800

    one change `
    make one funtion ,test github commit

--- atest.si4project/Backup/test(1041).c
+++ atest.si4project/Backup/test(1041).c
@@ -1,21 +1,5 @@
 #include 'stdio.h'
-//2017/12/21 
-//
-int fun()
-{
-	int i=0;
-	int j = 0;
 
-	i++;
-
-	if(i)
-	{
-		printf("fun: i = %d,j = %d \n", i, j);	
-	}
-
-	return i;
-	
-}
 int main()
 {
 	//这是一个测试用例
